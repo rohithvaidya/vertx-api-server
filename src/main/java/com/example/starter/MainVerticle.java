facc0203fba9ff0d989baa6b40e40a2252373521
Rohith <rohithvaidya@gmail.com>
2025-01-07 16:15:22 +0530

    added rest api test using restassured

--- src/main/java/com/example/starter/MainVerticle.java
+++ src/main/java/com/example/starter/MainVerticle.java
@@ -3,11 +3,13 @@ package com.example.starter;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
 import io.vertx.ext.web.Router;
-import java.util.*;  
 
+import java.util.*;  
 import java.util.Base64;
 
 public class MainVerticle extends AbstractVerticle {
+
+  String username;
 
   @Override
   public void start(Promise<Void> startPromise) throws Exception {
@@ -37,20 +39,19 @@ public class MainVerticle extends AbstractVerticle {
 
     router.get("/get-endpoint").handler(ctx -> {
 
-      String auth_header = ctx.request().getHeader("Authorization");
-      System.out.println(auth_header);
-      String base64Credentials = auth_header.substring("Basic ".length()).trim();
-      System.out.println(base64Credentials);
-      String credentials = new String(java.util.Base64.getDecoder().decode(base64Credentials));
-      System.out.println(credentials);
-      String username = credentials.split(":")[0];
-
-
+      if(ctx.request().getHeader("Authorization") != null){
+        String auth_header = ctx.request().getHeader("Authorization");
+        System.out.println(auth_header);
+        String base64Credentials = auth_header.substring("Basic ".length()).trim();
+        System.out.println(base64Credentials);
+        String credentials = new String(java.util.Base64.getDecoder().decode(base64Credentials));
+        System.out.println(credentials);
+        username = credentials.split(":")[0];
+      }
+    
       ctx.response()
         .putHeader("content-type", "text/plain")
         .end("Hi " + username + " GET request successful!");
-
-      
 
     });
 
--- src/test/java/com/example/starter/TestMainVerticle.java
+++ src/test/java/com/example/starter/TestMainVerticle.java
@@ -4,8 +4,14 @@ import io.vertx.core.Vertx;
 import io.vertx.junit5.VertxExtension;
 import io.vertx.junit5.VertxTestContext;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
 
 @ExtendWith(VertxExtension.class)
 public class TestMainVerticle {
@@ -19,4 +25,23 @@ public class TestMainVerticle {
   void verticle_deployed(Vertx vertx, VertxTestContext testContext) throws Throwable {
     testContext.completeNow();
   }
+
+  @BeforeAll
+  public static void setup(){
+    RestAssured.baseURI = "http://localhost:8888/";
+  }
+
+  @Test
+  public void test_get_endpoint(){
+    Response response = RestAssured.given().auth().preemptive().basic("rohith", "rohith").get("/get-endpoint");
+    
+    //asserting whether status code is successful
+    assertThat(response.getStatusCode(), equalTo(200));
+
+  }
+
+
+  
+
+
 }
