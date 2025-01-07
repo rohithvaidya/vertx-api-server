a9385f159ce49b56f00c40fafa9fb1f0bff46743
Rohith <rohithvaidya@gmail.com>
2025-01-07 12:19:18 +0530

    added a sample get and post endpoint

--- src/main/java/com/example/starter/MainVerticle.java
+++ src/main/java/com/example/starter/MainVerticle.java
@@ -2,16 +2,36 @@ package com.example.starter;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
+import io.vertx.ext.web.Router;
+import java.util.*;  
+
+import java.util.Base64;
 
 public class MainVerticle extends AbstractVerticle {
 
   @Override
   public void start(Promise<Void> startPromise) throws Exception {
-    vertx.createHttpServer().requestHandler(req -> {
-      req.response()
+
+    Router router = Router.router(vertx);
+
+    //Using a post request to print the json request body
+    router.post("/post-endpoint").handler(ctx -> {
+      
+      ctx.request().bodyHandler(body -> {
+        String requestBody = body.toString();
+        ctx.response()
+          .putHeader("content-type", "application/json")
+          .end(requestBody);
+      });
+    });
+
+    router.get("/get-endpoint").handler(ctx -> {
+      ctx.response()
         .putHeader("content-type", "text/plain")
-        .end("Hi from Vert.x!");
-    }).listen(8888).onComplete(http -> {
+        .end("GET request successful!");
+    });
+
+    vertx.createHttpServer().requestHandler(router).listen(8888).onComplete(http -> {
       if (http.succeeded()) {
         startPromise.complete();
         System.out.println("HTTP server started on port 8888");
