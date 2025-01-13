870720549e105274fe2107d4accaae55cca942f7
Rohith <rohithvaidya@gmail.com>
2025-01-13 12:10:24 +0530

    added postgres using pg-client

--- src/main/java/com/example/starter/MainVerticle.java
+++ src/main/java/com/example/starter/MainVerticle.java
@@ -4,6 +4,15 @@ import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
 import io.vertx.ext.web.Router;
 
+import io.vertx.core.Vertx;
+import io.vertx.pgclient.PgConnectOptions; // For PostgreSQL connection options
+import io.vertx.sqlclient.PoolOptions; // For configuring the connection pool
+import io.vertx.sqlclient.SqlClient; // For the client interface
+import io.vertx.sqlclient.RowSet; // To handle the result set
+import io.vertx.sqlclient.Row; // To represent rows in the result set
+import io.vertx.pgclient.PgBuilder; // To build the PostgreSQL client
+import io.vertx.sqlclient.Pool;
+
 import java.util.*;  
 import java.util.Base64;
 
@@ -18,6 +27,43 @@ public class MainVerticle extends AbstractVerticle {
 
     //Using a post request to print the json request body
     router.post("/post-endpoint").handler(ctx -> {
+
+      PgConnectOptions connectOptions = new PgConnectOptions()
+        .setPort(5432)
+        .setHost("localhost")
+        .setDatabase("a_db")
+        .setUser("postgres")
+        .setPassword("");
+
+     
+      PoolOptions poolOptions = new PoolOptions()
+        .setMaxSize(5);
+
+     
+      Pool client = Pool.pool(vertx, connectOptions, poolOptions);
+
+    
+      client
+        .query("SELECT * FROM a")
+        .execute()
+        .onComplete(ar -> {
+          if (ar.succeeded()) {
+            RowSet<Row> result = ar.result();
+            for (Row row : result) {
+        // Access column values by name
+        String column1 = row.getString("name");
+        Integer column2 = row.getInteger("id");
+
+        // Print row data
+        System.out.println("id " + column2 + ", name " + column1);
+      }
+          } else {
+            System.out.println("Failure: " + ar.cause().getMessage());
+          }
+
+          // Now close the pool
+          client.close();
+        });
 
       String auth_header = ctx.request().getHeader("Authorization");
       System.out.println(auth_header);
